90fb3906adabc858626fe0c0ad693675ac5c3103
João Campos <joaon.c.jv@gmail.com>
2025-08-27 12:18:57 -0300

    feat: made the pagination feature

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -4,12 +4,27 @@ import Menu from "../components/Menu";
 import SearchComponent from "../components/SearchComponent";
 import { MovieService } from "../services/movieService";
 import ListMovies from "../components/ListMovies";
-import movieImage from "../assets/img/movie-image.png"
+import movieImage from "../assets/img/movie-image.png";
+import Pagination from "@mui/material/Pagination";
+import Stack from "@mui/material/Stack";
 
 const Search = () => {
   const [searchValueFormatted, setsearchValueFormatted] = useState("");
   const [searchValueNonFormatted, setsearchValueNonFormatted] = useState("");
   const [moviesList, setMoviesList] = useState([]);
+  const [maxPages, setMaxPages] = useState(5);
+
+  const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
+    const api = new MovieService();
+    api
+      .getMovies(
+        `https://api.themoviedb.org/3/search/movie?query=${searchValueFormatted}&language=pt-BR&page=${value}`
+      )
+      .then((data) => {
+        setMoviesList(data.results);
+        data.total_pages > 5 ? setMaxPages(5) : setMaxPages(data.total_pages);
+      });
+  };
 
   const formatValue = (value: string) => {
     let removeSpaces = value.trim().replace(/\s+/g, " ");
@@ -18,7 +33,7 @@ const Search = () => {
   };
 
   const handleTargetValue = (value: string) => {
-    setsearchValueNonFormatted(value.trim())
+    setsearchValueNonFormatted(value.trim());
     const valueFormatted = formatValue(value);
     setsearchValueFormatted(valueFormatted);
   };
@@ -29,7 +44,10 @@ const Search = () => {
       .getMovies(
         `https://api.themoviedb.org/3/search/movie?query=${searchValueFormatted}&language=pt-BR`
       )
-      .then((data) => setMoviesList(data.results));
+      .then((data) => {
+        setMoviesList(data.results);
+        setMaxPages(data.total_pages);
+      });
   };
 
   // useEffect(() => {
@@ -49,14 +67,35 @@ const Search = () => {
         />
         {moviesList.length === 0 ? (
           <div className="flex flex-col gap-4 items-center justify-center h-[calc(100vh-320px)]">
-            <img src={movieImage} alt="movie image" className="w-48"/>
-            <h2 className="text-xl font-semibold px-4 text-center">Nenhum resultado para sua pesquisa ainda. Pesquise seu filme
-            favorito!</h2>
+            <img src={movieImage} alt="movie image" className="w-48" />
+            <h2 className="text-xl font-semibold px-4 text-center">
+              Nenhum resultado para sua pesquisa ainda. Pesquise seu filme
+              favorito!
+            </h2>
           </div>
         ) : (
           <>
-          <h2 className="text-xl font-light text-center p-4">Resultado para: <strong className="font-bold">{searchValueNonFormatted}</strong></h2>
-          <ListMovies moviesList={moviesList} />
+            <h2 className="text-xl font-light text-center p-4">
+              Pesquisando por:
+              <strong className="font-bold"> {searchValueNonFormatted}</strong>
+            </h2>
+            <ListMovies moviesList={moviesList} />
+            {maxPages === 1 ? (
+              <></>
+            ) : (
+              <Stack spacing={2} className="m-auto w-fit py-8">
+                <Pagination
+                  count={maxPages}
+                  onChange={handleChange}
+                  color="secondary"
+                  sx={{
+                    "& .MuiPaginationItem-root": {
+                      color: "white",
+                    },
+                  }}
+                />
+              </Stack>
+            )}
           </>
         )}
       </main>
